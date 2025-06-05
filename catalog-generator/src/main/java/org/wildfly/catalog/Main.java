65a95c5ecdf896590aad557d2492f7b38077a8e8
Jean-Francois Denise <jdenise@redhat.com>
2025-06-05 14:22:56 +0200

    Catalog built from fp

--- catalog-generator/src/main/java/org/wildfly/catalog/Main.java
+++ catalog-generator/src/main/java/org/wildfly/catalog/Main.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -50,128 +51,42 @@ public class Main {
         target.put("description", "Catalog of WildFly " + wildflyVersion + " features (incomplete for now) to provision a WildFly server");
         target.set("documentation", node.get("documentation"));
         target.set("legend", node.get("legend"));
-        
+
         // Glow rules description
         Properties glowRulesDescriptions = new Properties();
         String rulesURL = node.get("glowRulesDescriptions").asText();
-        try(InputStream in = new URL(rulesURL).openStream()) {
+        try (InputStream in = new URL(rulesURL).openStream()) {
             glowRulesDescriptions.load(in);
         }
-        ArrayNode an = (ArrayNode) node.get("content");
-        Iterator<JsonNode> it = an.elements();
         Map<String, Map<String, JsonNode>> categories = new TreeMap<>();
-        while (it.hasNext()) {
-            JsonNode remoteMetadata = it.next();
-            String metadataUrl = remoteMetadata.get("url").asText();
-//            JsonNode subCatalogMetadata = mapper.readTree(new URL(metadataUrl));
-//            ArrayNode wildflyVersions = (ArrayNode) subCatalogMetadata.get("wildfly-versions");
-//            Iterator<JsonNode> versions = wildflyVersions.elements();
-//            String url = null;
-//            while (versions.hasNext()) {
-//                JsonNode wfVersionNode = versions.next();
-//                String vers = wfVersionNode.get("wildfly-version").asText();
-//                if (vers.equals(wildflyVersion)) {
-//                    url = wfVersionNode.get("url").asText();
-//                    break;
-//                }
-//            }
-//            if (url == null) {
-//                // Do we try with the latest one defined in the latest version?
-//                System.err.println("Skipping " + remoteMetadata.get("description") + " no matching metadata for version " + wildflyVersion);
-//                continue;
-//            }
-            JsonNode subCatalog = mapper.readTree(new URL(metadataUrl));
-            String fp = subCatalog.get("feature-pack-location").asText();
-
-            ArrayNode layersArray = (ArrayNode) subCatalog.get("layers");
-            Iterator<JsonNode> layers = layersArray.elements();
-            while (layers.hasNext()) {
-                ObjectNode layer = (ObjectNode) layers.next();
-                layer.put("feature-pack", fp);
-                String layerName = layer.get("name").asText();
-                Iterator<JsonNode> properties = ((ArrayNode) layer.get("properties")).elements();
-                String category = null;
-                String description = null;
-                String note = null;
-                String addOn = null;
-                String stability = null;
-                List<JsonNode> discoveryRules = new ArrayList<>();
-                while (properties.hasNext()) {
-                    ObjectNode prop = (ObjectNode) properties.next();
-                    String name = prop.get("name").asText();
-                    if (name.equals("org.wildfly.category")) {
-                        category = prop.get("value").asText();
-                        continue;
-                    }
-                    if (name.equals("org.wildfly.description")) {
-                        description = prop.get("value").asText();
-                        continue;
-                    }
-                    if (name.equals("org.wildfly.note")) {
-                        note = prop.get("value").asText();
-                        continue;
-                    }
-                    if (name.equals("org.wildfly.stability")) {
-                        stability = prop.get("value").asText();
-                        continue;
-                    }
-                    if (name.equals("org.wildfly.rule.add-on")) {
-                        String val = prop.get("value").asText();
-                        addOn = val.split(",")[1];
-                        continue;
-                    }
-                    if (name.equals("org.wildfly.rule.kind")) {
-                        String val = prop.get("value").asText();
-                        if (val.equals("default-base-layer")) {
-                            discoveryRules.add(prop);
-                            setRuleDescription(name, glowRulesDescriptions, prop);
-                        }
-                        continue;
-                    }
-                    if (name.startsWith("org.wildfly.rule") && !name.startsWith("org.wildfly.rule.add-on")) {
-                        discoveryRules.add(prop);
-                        setRuleDescription(name, glowRulesDescriptions, prop);
-                        continue;
-                    }
-                }
-                layer.remove("properties");
-                if (category == null) {
-                    throw new Exception("Invalid format, org.wildfly.category is missing for layer " + layer.get("name"));
-                }
-                if (description != null) {
-                    layer.put("description", description);
-                }
-                if (note != null) {
-                    layer.put("note", note);
-                }
-                if (addOn != null) {
-                    layer.put("glowAddOn", addOn);
-                }
-                if (stability == null) {
-                    layer.put("stability", "default");
-                } else {
-                    layer.put("stability", stability);
-                }
-                if (!discoveryRules.isEmpty()) {
-                    ArrayNode rules = mapper.createArrayNode();
-                    rules.addAll(discoveryRules);
-                    layer.putIfAbsent("glowRules", rules);
-                }
-                layer.put("glowDiscoverable", !discoveryRules.isEmpty());
-                Map<String, JsonNode> nodes = categories.get(category);
-                if (nodes == null) {
-                    nodes = new TreeMap<>();
-                    categories.put(category, nodes);
-                }
-                if (nodes.containsKey(layerName)) {
-                    // add all dependencies
-                    System.out.println("OVERRIDEN !!!!! " + layerName);
-                    JsonNode overriden = nodes.get(layerName);
-                    ArrayNode deps = (ArrayNode) layer.get("dependencies");
-                    ArrayNode overridenDeps = (ArrayNode) overriden.get("dependencies");
-                    deps.addAll(overridenDeps);
-                }
-                nodes.put(layerName, layer);
+        // Old way, based on URL online.
+        if (node.has("content")) {
+            ArrayNode an = (ArrayNode) node.get("content");
+            Iterator<JsonNode> it = an.elements();
+            while (it.hasNext()) {
+                JsonNode remoteMetadata = it.next();
+                String metadataUrl = remoteMetadata.get("url").asText();
+                JsonNode subCatalog = mapper.readTree(new URL(metadataUrl));
+                generateCatalog(subCatalog, glowRulesDescriptions, categories, mapper);
+            }
+        } else {
+            String url = node.get("knownFeaturePacks").asText();
+            JsonNode fpList = mapper.readTree(new URL(url));
+            ArrayNode fps = (ArrayNode) fpList.get("featurePacks");
+            Iterator<JsonNode> it = fps.elements();
+            while (it.hasNext()) {
+                String fp = it.next().asText();
+                System.out.println("FP " + fp);
+                // Mimic what we would do using maven. here we expect all to be in the cache
+                Path home = Paths.get(System.getProperty("user.home")).resolve(".m2").resolve("repository");
+                String[] coords = fp.split(":");
+                String groupId = coords[0].replaceAll("\\.", File.separator);
+                String artifactId = coords[1];
+                String version = coords[2];
+                String metadataFileName = artifactId + "-"+version+"-metadata.json";
+                Path metadataFile = home.resolve(groupId).resolve(artifactId).resolve(version).resolve(metadataFileName);
+                JsonNode subCatalog = mapper.readTree(metadataFile.toFile().toURI().toURL());
+                generateCatalog(subCatalog, glowRulesDescriptions, categories, mapper);
             }
         }
         ArrayNode categoriesArray = mapper.createArrayNode();
@@ -241,13 +156,109 @@ public class Main {
         }
     }
 
+    private static void generateCatalog(JsonNode subCatalog, Properties glowRulesDescriptions,
+            Map<String, Map<String, JsonNode>> categories, ObjectMapper mapper) throws Exception {
+        String fp = subCatalog.get("feature-pack-location").asText();
+
+        ArrayNode layersArray = (ArrayNode) subCatalog.get("layers");
+        Iterator<JsonNode> layers = layersArray.elements();
+        while (layers.hasNext()) {
+            ObjectNode layer = (ObjectNode) layers.next();
+            layer.put("feature-pack", fp);
+            String layerName = layer.get("name").asText();
+            Iterator<JsonNode> properties = ((ArrayNode) layer.get("properties")).elements();
+            String category = null;
+            String description = null;
+            String note = null;
+            String addOn = null;
+            String stability = null;
+            List<JsonNode> discoveryRules = new ArrayList<>();
+            while (properties.hasNext()) {
+                ObjectNode prop = (ObjectNode) properties.next();
+                String name = prop.get("name").asText();
+                if (name.equals("org.wildfly.category")) {
+                    category = prop.get("value").asText();
+                    continue;
+                }
+                if (name.equals("org.wildfly.description")) {
+                    description = prop.get("value").asText();
+                    continue;
+                }
+                if (name.equals("org.wildfly.note")) {
+                    note = prop.get("value").asText();
+                    continue;
+                }
+                if (name.equals("org.wildfly.stability")) {
+                    stability = prop.get("value").asText();
+                    continue;
+                }
+                if (name.equals("org.wildfly.rule.add-on")) {
+                    String val = prop.get("value").asText();
+                    addOn = val.split(",")[1];
+                    continue;
+                }
+                if (name.equals("org.wildfly.rule.kind")) {
+                    String val = prop.get("value").asText();
+                    if (val.equals("default-base-layer")) {
+                        discoveryRules.add(prop);
+                        setRuleDescription(name, glowRulesDescriptions, prop);
+                    }
+                    continue;
+                }
+                if (name.startsWith("org.wildfly.rule") && !name.startsWith("org.wildfly.rule.add-on")) {
+                    discoveryRules.add(prop);
+                    setRuleDescription(name, glowRulesDescriptions, prop);
+                    continue;
+                }
+            }
+            layer.remove("properties");
+            if (category == null) {
+                throw new Exception("Invalid format, org.wildfly.category is missing for layer " + layer.get("name"));
+            }
+            if (description != null) {
+                layer.put("description", description);
+            }
+            if (note != null) {
+                layer.put("note", note);
+            }
+            if (addOn != null) {
+                layer.put("glowAddOn", addOn);
+            }
+            if (stability == null) {
+                layer.put("stability", "default");
+            } else {
+                layer.put("stability", stability);
+            }
+            if (!discoveryRules.isEmpty()) {
+                ArrayNode rules = mapper.createArrayNode();
+                rules.addAll(discoveryRules);
+                layer.putIfAbsent("glowRules", rules);
+            }
+            layer.put("glowDiscoverable", !discoveryRules.isEmpty());
+            Map<String, JsonNode> nodes = categories.get(category);
+            if (nodes == null) {
+                nodes = new TreeMap<>();
+                categories.put(category, nodes);
+            }
+            if (nodes.containsKey(layerName)) {
+                // add all dependencies
+                System.out.println("OVERRIDEN !!!!! " + layerName);
+                JsonNode overriden = nodes.get(layerName);
+                ArrayNode deps = (ArrayNode) layer.get("dependencies");
+                ArrayNode overridenDeps = (ArrayNode) overriden.get("dependencies");
+                deps.addAll(overridenDeps);
+            }
+            nodes.put(layerName, layer);
+        }
+    }
+
     private static void setRuleDescription(String name, Properties props, ObjectNode node) {
-        for(String k : props.stringPropertyNames()) {
-            if(name.startsWith(k)) {
+        for (String k : props.stringPropertyNames()) {
+            if (name.startsWith(k)) {
                 node.put("ruleDescription", props.getProperty(k));
-                node.put("valueDescription", props.getProperty(k+".value"));
+                node.put("valueDescription", props.getProperty(k + ".value"));
                 break;
             }
         }
-    } 
+    }
 }
